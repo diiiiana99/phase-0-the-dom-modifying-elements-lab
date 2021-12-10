e0caf29a4c4180dc086a9bd54be98f0ff32ecd84
diiiiana99 <93669061+diiiiana99@users.noreply.github.com>
2021-12-10 09:37:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+move.remove(1)
+
+const newHeader = document.createElement("h1");
+newHeader . id ='victory';
+newHeader . innerHTML = "Diana, you got this";
+document.body.append(newHeader);
